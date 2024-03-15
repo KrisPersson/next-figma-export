636a707722ce46a1538539bfc12db0d2903f51df
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-03-15 13:13:15 +0100

    hero

--- src/app/ui/small-cards-container.tsx
+++ src/app/ui/small-cards-container.tsx
@@ -6,35 +6,37 @@ import { products } from "../products";
 
 export default async function SmallCardsContainer() {
   return (
-    <section className={styles.container}>
-      {products.map((product, i) => {
-        return (
-          <article key={i} className={styles.smallCard}>
-            <section className={styles.imageSection}>
-              <Image
-                src={product.productImgSrc}
-                alt={`Image of product named ${product.name}`}
-                fill={true}
-                className={styles.productImg}
-              ></Image>
-            </section>
-            <section className={styles.infoSection}>
-              <h3 className={styles.typeTitle}>{product.type}</h3>
-              <h2 className={styles.nameTitle}>{product.name}</h2>
-              <Image
-                src={product.featureDotImgSrc}
-                className={styles.circles}
-                width={126}
-                height={16}
-                alt="Product features"
-              ></Image>
-              <Link className={styles.shopNow} href="/">
-                Shop now
-              </Link>
-            </section>
-          </article>
-        );
-      })}
+    <section className={styles.productSection}>
+      <div className={styles.container}>
+        {products.map((product, i) => {
+          return (
+            <article key={i} className={styles.smallCard}>
+              <section className={styles.imageSection}>
+                <Image
+                  src={product.productImgSrc}
+                  alt={`Image of product named ${product.name}`}
+                  fill={true}
+                  className={styles.productImg}
+                ></Image>
+              </section>
+              <section className={styles.infoSection}>
+                <h3 className={styles.typeTitle}>{product.type}</h3>
+                <h2 className={styles.nameTitle}>{product.name}</h2>
+                <Image
+                  src={product.featureDotImgSrc}
+                  className={styles.circles}
+                  width={126}
+                  height={16}
+                  alt="Product features"
+                ></Image>
+                <Link className={styles.shopNow} href="/">
+                  Shop now
+                </Link>
+              </section>
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 }
